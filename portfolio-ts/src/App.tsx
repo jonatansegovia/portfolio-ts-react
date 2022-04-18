512e4455e896da5d156b03bb7e7a9c6796a0b219
jonatan segovia <jsegovia.ush@gmail.com>
2022-04-18 19:21:52 -0300

    mui added

--- portfolio-ts/src/App.tsx
+++ portfolio-ts/src/App.tsx
@@ -1,8 +1,37 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Typed from 'react-typed';
+
 import './App.css';
-import Button from '@mui/material/Button';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Grid container className="App">
+      <Grid item>
+        <Typography variant="h1" align="center">
+          Hi! I'm J0n
+        </Typography>
+        <Typography variant="h1" align="center">
+          The
+        </Typography>
+        <Typography variant="h2" align="center">
+          <Typed
+            strings={[
+              'Developer',
+              'Father',
+              'Self-Taught',
+              'Messi Fan',
+              'React Lover',
+              'Empathetic',
+            ]}
+            typeSpeed={70}
+            backSpeed={50}
+            loop
+          />
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
